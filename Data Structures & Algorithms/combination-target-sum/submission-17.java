038c473df8c731f5aa89e786a1f17e877698825b
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-04-22 08:56:16 -0400

    Add: combination-target-sum - submission-17

---
+++ Data Structures & Algorithms/combination-target-sum/submission-17.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> combo = new ArrayList<>();
+
+        backtrack(nums, target, 0, combo, res);
+        
+        return res;
+    }
+
+    private void backtrack(int[] nums, int target, int index, List<Integer> combo, List<List<Integer>> res){
+        if(target == 0){
+            res.add(new ArrayList<>(combo));
+            return;
+        }
+
+        if(target < 0 || nums.length == index){
+            return;
+        }
+
+        combo.add(nums[index]);
+        backtrack(nums, target - nums[index], index, combo, res);
+
+        combo.remove(combo.size() - 1);
+        backtrack(nums, target, index + 1, combo, res);
+    }
+}
