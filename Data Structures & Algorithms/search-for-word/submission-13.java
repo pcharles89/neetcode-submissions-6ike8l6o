cb0ad4bff00df4e2f134e4f9ad6ed24783cc2832
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-04-26 16:33:06 -0400

    Add: search-for-word - submission-13

---
+++ Data Structures & Algorithms/search-for-word/submission-13.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                if(dfs(board, word, 0, r, c)){
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+
+    private boolean dfs(char[][] board, String word, int i, int r, int c){
+        if(i == word.length()){
+            return true;
+        }
+
+        if(r < 0 || c < 0 || r >= board.length || c >= board[0].length){
+            return false;
+        }
+
+        if(board[r][c] != word.charAt(i)){
+            return false;
+        }
+
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        boolean found = dfs(board, word, i + 1, r + 1, c) ||
+                        dfs(board, word, i + 1, r - 1, c) ||
+                        dfs(board, word, i + 1, r, c + 1) ||
+                        dfs(board, word, i + 1, r, c - 1);
+        
+        board[r][c] = temp;
+
+        return found;
+    }
+}
