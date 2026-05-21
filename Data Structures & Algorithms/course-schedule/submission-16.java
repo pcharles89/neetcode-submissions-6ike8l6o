aa3586a8f66585f4981b37796ab50a39924ec253
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-05-21 08:46:42 -0400

    Add: course-schedule - submission-16

---
+++ Data Structures & Algorithms/course-schedule/submission-16.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> preMap = new HashMap<>();
+
+        for(int i = 0; i < numCourses; i++){
+            preMap.put(i, new ArrayList<>());
+        }
+
+        for(int[] prereq : prerequisites){
+            int course = prereq[0];
+            int prerequisite = prereq[1];
+            preMap.get(course).add(prerequisite);
+        }
+
+        Set<Integer> visiting = new HashSet<>();
+
+        for(int course = 0; course < numCourses; course++){
+            if(!dfs(course, preMap, visiting)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int course, Map<Integer, List<Integer>> preMap, Set<Integer> visiting){
+        if(visiting.contains(course)){
+            return false;
+        }
+
+        if(preMap.get(course).isEmpty()){
+            return true;
+        }
+
+        visiting.add(course);
+
+        for(int prereq : preMap.get(course)){
+            if(!dfs(prereq, preMap, visiting)){
+                return false;
+            }
+        }
+
+        visiting.remove(course);
+        preMap.put(course, new ArrayList<>());
+
+        return true;
+    }
+}
