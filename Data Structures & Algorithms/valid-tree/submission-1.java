6736905a7a23f30f350ae2779c2fefa7bd40b9b0
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-05-22 07:03:51 -0400

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length != n - 1){
+            return false;
+        }
+
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+
+        for(int i = 0; i < n; i++){
+            adj.put(i, new ArrayList<>());
+        }
+
+        for(int[] edge : edges){
+            int node1 = edge[0];
+            int node2 = edge[1];
+
+            adj.get(node1).add(node2);
+            adj.get(node2).add(node1);
+        }
+
+        dfs(0, adj, visited);
+
+        return visited.size() == n;
+
+    }
+
+    private void dfs(int node, Map<Integer, List<Integer>> adj, Set<Integer> visited){
+        if(visited.contains(node)){
+            return;
+        }
+
+        visited.add(node);
+
+        for(int neighbor : adj.get(node)){
+            dfs(neighbor, adj, visited);
+        }
+    }
+}
