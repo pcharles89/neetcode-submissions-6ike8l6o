8074bd88b3712a5d0ab14de85b1caced13db5287
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-06-03 09:11:05 -0400

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+
+        UnionFind uf = new UnionFind(n + 1);
+
+        for(int[] edge : edges){
+            int a = edge[0];
+            int b = edge[1];
+
+            if(!uf.union(a, b)){
+                return edge;
+            }
+        }
+
+        return new int[0];
+    }
+
+    class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int n){
+            parent = new int[n];
+            rank = new int[n];
+
+            for(int i = 0; i < n; i++){
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        private int find(int x){
+            if(parent[x] != x){
+                parent[x] = find(parent[x]);
+            }
+
+            return parent[x];
+        }
+
+        public boolean union(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if(rootX == rootY){
+                return false;
+            }
+
+            if(rank[rootX] > rank[rootY]){
+                parent[rootY] = rootX;
+            }
+
+            else if(rank[rootX] < rank[rootY]){
+                parent[rootX] = rootY;
+            }
+
+            else{
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+
+            return true;
+        }
+
+        
+        
+    }
+}
