86d220584f99f9df777f296c646e27a608d6a9b4
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-06-04 07:47:41 -0400

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.java
@@ -0,0 +1,63 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1) {
+            return false;
+        }
+
+        UnionFind uf = new UnionFind(n);
+
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+
+            if (!uf.union(a, b)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int node) {
+            if (parent[node] != node) {
+                parent[node] = find(parent[node]);
+            }
+
+            return parent[node];
+        }
+
+        public boolean union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+
+            if (rootA == rootB) {
+                return false;
+            }
+
+            if (rank[rootA] > rank[rootB]) {
+                parent[rootB] = rootA;
+            } else if (rank[rootA] < rank[rootB]) {
+                parent[rootA] = rootB;
+            } else {
+                parent[rootB] = rootA;
+                rank[rootA]++;
+            }
+
+            return true;
+        }
+    }
+}
