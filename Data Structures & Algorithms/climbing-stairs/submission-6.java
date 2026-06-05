6bd03dc33eb4cee4bc48baf1746a09b784e38283
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-06-05 07:07:52 -0400

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+
+        int one = 1;
+        int two = 2;
+
+        for(int i = 3; i <= n; i++){
+            int temp = two;
+            two = one + two;
+            one = temp;
+        }
+
+        return two;
+    }
+}
