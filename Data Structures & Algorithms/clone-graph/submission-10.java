84b86e6a1734b202b08a6d9cf40e617a4c1aa41e
pcharles89 <93538393+pcharles89@users.noreply.github.com>
2026-08-28 06:44:57 -0400

    Add: clone-graph - submission-10

---
+++ Data Structures & Algorithms/clone-graph/submission-10.java
@@ -0,0 +1,23 @@
+class Solution {
+    Map<Node, Node> oldToNew = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        
+        if(node == null){
+            return null;
+        }
+
+        if(oldToNew.containsKey(node)){
+            return oldToNew.get(node);
+        }
+
+        Node copy = new Node(node.val);
+        oldToNew.put(node, copy);
+
+        for(Node neighbor : node.neighbors){
+            copy.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return copy;
+    }
+}
